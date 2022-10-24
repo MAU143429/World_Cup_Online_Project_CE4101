d6fb5d422d11e6a5bb535d30cc6c5d653bff30c5
MAU <mau141226@gmail.com>
2022-10-23 21:58:29 -0600

    [Update] create-tournament view updated
    Routing module updated
    Bootstrap, ng material and mobiscroll added

--- WCO_WEB/WCO/src/app/components/create-tournament/create-tournament.component.ts
+++ WCO_WEB/WCO/src/app/components/create-tournament/create-tournament.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
+import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
 
+export interface bracketName {
+  name: string;
+}
 @Component({
   selector: 'app-create-tournament',
   templateUrl: './create-tournament.component.html',
-  styleUrls: ['./create-tournament.component.css']
+  styleUrls: ['./create-tournament.component.css'],
 })
 export class CreateTournamentComponent implements OnInit {
+  myData = [
+    { text: 'Books', value: 1 },
+    { text: 'Movies, Music & Games', value: 2 },
+    { text: 'Electronics & Computers', value: 3 },
+    { text: 'Home, Garden & Tools', value: 4 },
+    { text: 'Health & Beauty', value: 5 },
+    { text: 'Toys, Kids & Baby', value: 6 },
+    { text: 'Clothing & Jewelry', value: 7 },
+    { text: 'Sports & Outdoors', value: 8 },
+    { text: 'Automotive & Industrial', value: 9 },
+  ];
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'action'];
+  bracketSource: bracketName[] = [];
 
-  ngOnInit(): void {
+  @ViewChild(MatTable, { static: true }) table: MatTable<any> | any;
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  openDialog(action: String, obj: any) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogBoxComponent, {
+      width: '250px',
+      data: obj,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result.event == 'Agregar') {
+        this.addRowData(result.data);
+      } else if (result.event == 'Editar') {
+        this.updateRowData(result.data);
+      } else if (result.event == 'Eliminar') {
+        this.deleteRowData(result.data);
+      }
+    });
   }
 
+  addRowData(row_obj: any) {
+    this.bracketSource.push({
+      name: row_obj.name,
+    });
+    this.table.renderRows();
+  }
+  updateRowData(row_obj: any) {
+    this.bracketSource = this.bracketSource.filter((value, key) => {
+      value.name = row_obj.name;
+      return true;
+    });
+  }
+  deleteRowData(row_obj: any) {
+    this.bracketSource = this.bracketSource.filter((value, key) => {
+      return value != row_obj;
+    });
+  }
 }
