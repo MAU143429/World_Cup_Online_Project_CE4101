1567dc8b4b13134d1b6ce3b0a32e7dadcfa44c37
juanprrr <54974335+juanprrr@users.noreply.github.com>
2022-10-24 17:13:35 -0600

    Revert "[Add] Server requests environment added"
    This reverts commit 5fd191d9a42aae0b31929a8fc8b06fbc621a3fbf.

--- WCO_WEB/WCO/src/app/services/env-url.service.ts
+++ WCO_WEB/WCO/src/app/services/env-url.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
-/**
- * @class Contains api url address for handling server requests from frontend
- */
 export class EnvUrlService {
- public urlAddress: string = environment.apiUrl;
+
   constructor() { }
 }
