7a792a82230740d98caf9b884aad093bb4bc94b7
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2022-10-24 15:46:23 -0600

    [Add] Tournament controller and model

---
+++ WCO_API/WCO_Api/Models/Tournament.cs
@@ -0,0 +1,23 @@
+﻿namespace WCO_Api.Models
+{
+    public class Tournament
+    {
+        // Revisar como se tratará las varas de los ID, usando los UUID o como se hace esto en la 
+        //base de datos
+        public string? Id { get; set; }
+
+        public string name { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+
+        public string description { get; set; }
+
+        public string type { get; set; }
+
+        public List<string> teams { get; set; }
+
+        public List<string> brackets { get; set; }
+    }
+}
