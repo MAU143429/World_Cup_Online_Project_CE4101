961edee1ab109471b5ebe668080f45e29e917a74
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2022-10-25 09:19:40 -0600

    [Add] Post method match

---
+++ WCO_API/WCO_Api/Controllers/MatchController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WCO_Api.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+
+        [HttpPost]
+        
+        public async Task<ActionResult<Match>> Post(Match match)
+        {
+            return match;
+        }
+
+    }
+}
---
+++ WCO_API/WCO_Api/Models/Match.cs
@@ -0,0 +1,19 @@
+﻿namespace WCO_Api.Models
+{
+    public class Match
+    {
+        public string Id { get; set; }
+
+        public string tournamentName { get; set; }
+
+        public string phaseName { get; set; }
+
+        public string venue { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public string team1 { get; set; }
+
+        public string team2 { get; set; }
+    }
+}
