421cf24ba11910038008b2be1d1950abe3fd821f
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2022-10-26 11:31:49 -0600

    [Added] Models for DB

---
+++ WCO_API/WCO_Api/Models/Match.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WCO_Api.Models
+{
+    public partial class Match
+    {
+        public Match()
+        {
+            MatchTeams = new HashSet<MatchTeam>();
+        }
+
+        public int MId { get; set; }
+        public string StartTime { get; set; } = null!;
+        public string Date { get; set; } = null!;
+        public string Venue { get; set; } = null!;
+        public int BracketId { get; set; }
+
+        public virtual Bracket Bracket { get; set; } = null!;
+        public virtual ICollection<MatchTeam> MatchTeams { get; set; }
+    }
+}
---
+++ WCO_API/WCO_Api/Models/MatchTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WCO_Api.Models
+{
+    public partial class MatchTeam
+    {
+        public int Id { get; set; }
+        public int TeamId { get; set; }
+        public int MatchId { get; set; }
+
+        public virtual Match Match { get; set; } = null!;
+        public virtual Team Team { get; set; } = null!;
+    }
+}
