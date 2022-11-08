7ad52694addc1897d993cbedb4ac655c42380a9c
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2022-11-07 21:58:50 -0600

    [Update] Mapping solved

---
+++ WCO_API/WCO_Api/WEBModels/TeamOut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WCO_Api.WEBModels
+{
+    public partial class TeamOut
+    {
+
+        public int TeId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string? TournamentId { get; set; }
+
+
+    }
+}
