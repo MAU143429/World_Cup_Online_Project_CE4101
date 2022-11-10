5ea733b007b03ce07f1e072e055ca0b85fabcba3
MAU <mau141226@gmail.com>
2022-11-09 19:06:45 -0600

    [Update] Some bugs fixed in verifications methods

--- WCO_WEB/WCO/src/app/components/register/register.component.ts
+++ WCO_WEB/WCO/src/app/components/register/register.component.ts
@@ -4,6 +4,7 @@ import countriesData from '../../../assets/scripts/countries.json';
 import { CreateAccount } from 'src/app/model/create-account';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AccountService } from '../../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,33 +15,59 @@ export class RegisterComponent implements OnInit {
   Users: CreateAccount[] = [];
   newAccount: CreateAccount = new CreateAccount();
   age: number = 0;
+  checkboxStatus: Boolean = false;
   constructor(
     private service: AccountService,
-    private _snackBar: MatSnackBar) {}
+    private _snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   /**
    * Este metodo permite realizar un pequeño delay
    * @param ms el tiempo del delay en ms
    */
-   async delay(ms: number) {
+  async delay(ms: number) {
     await new Promise<void>((resolve) => setTimeout(() => resolve(), ms)).then(
       () => console.log('fired')
     );
-  }  
+  }
 
   ngOnInit(): void {}
 
   CData: any[] = countriesData;
-  
 
-  openError(message: string) {
-    this._snackBar.open(message, 'Intente de nuevo',{
+  openError(message: string, message2: string) {
+    this._snackBar.open(message, message2, {
       duration: 2000,
       horizontalPosition: 'center',
       verticalPosition: 'top',
-      panelClass: 'red-snackbar',      
-    })
-  };
+      panelClass: 'red-snackbar',
+    });
+  }
+
+  openSuccess(message1: string, message2: string) {
+    this._snackBar.open(message1, message2, {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: 'green-snackbar',
+    });
+  }
+
+  CalculateAge() {
+    if (this.newAccount.birthdate) {
+      var timeDiff = Math.abs(
+        Date.now() - new Date(this.newAccount.birthdate).getTime()
+      );
+      this.age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+
+      console.log(this.age);
+    }
+  }
+
+  changeStatus() {
+    this.checkboxStatus = !this.checkboxStatus;
+  }
 
   /**
    * Este metodo permite realizar las verificaciones del formulario de crear
@@ -48,7 +75,7 @@ export class RegisterComponent implements OnInit {
    */
   addUser() {
     console.table(this.newAccount);
-  
+
     this.delay(50).then(() => {
       this.service
         .getAccountByEmail(this.newAccount.email)
@@ -56,65 +83,44 @@ export class RegisterComponent implements OnInit {
     });
 
     //Verificar que esten todos los espacios requeridos
-    if (!(this.newAccount.email.length > 0 && this.newAccount.password.length > 0
-        && this.newAccount.name.length > 0 && this.newAccount.lastname.length > 0
-        && this.newAccount.nickname.length > 0  && this.newAccount.birthdate.length > 0 
-        && this.newAccount.country.length > 0)){
-      this.openError("Faltan espacios requeridos para registrarse")
-    }
-
-    else{ 
-      this.CalculateAge()
-    }
-      
-
-    // Verificar formato y existencia de la cuenta con este corre
-    
-    // Faltaria revisar formato de correo
-
-    if (this.Users.length > 0){
-      this.openError("Hay una cuenta existente con este correo")
-    }
-
-    else if (!(this.newAccount.name.length < 30)){
-      this.openError("Nombre excede 30 caracteres permitidos")
-    }
-
-    else if (!(this.newAccount.lastname.length < 30)){
-      this.openError("Apellido excede 30 caracteres permitidos")
-    }
-
-  
-    // Verificar extension y formato de contraseña
-    
-    else if (!(this.newAccount.password.length >= 6 && this.newAccount.password.length <= 16)){
-      this.openError("Contraseña debe tener una extensión entre 6 y 16 caractéres")
-    }
-
-    // Verificar la fecha de nacimiento (mayor de 18)
-    // create account no deberia devolver la edad desde la base de datos como atributo derivado?
-
-    else if (!(this.age >= 18)){
-      this.openError("Debe ser mayor de 18 años")
-    }
-    
-    // Mandar a crear la cuenta
-    else{
-      this.service.createAccount(this.newAccount)
+    if (
+      this.newAccount.email == '' ||
+      this.newAccount.password == '' ||
+      this.newAccount.name == '' ||
+      this.newAccount.lastname == '' ||
+      this.newAccount.nickname == '' ||
+      this.newAccount.birthdate == '' ||
+      this.newAccount.country == '' ||
+      this.checkboxStatus == false
+    ) {
+      this.openError(
+        'Faltan espacios requeridos para registrarse',
+        'Intente de nuevo'
+      );
+    } else {
+      this.CalculateAge();
+      if (this.Users.length > 0) {
+        this.openError(
+          'Hay una cuenta existente con este correo',
+          'Ingrese otra para continuar'
+        );
+      } else if (
+        !(
+          this.newAccount.password.length >= 6 &&
+          this.newAccount.password.length <= 8
+        )
+      ) {
+        this.openError(
+          'La contraseña debe tener una extensión entre 6 y 8 caractéres',
+          'Intente de nuevo'
+        );
+      } else if (!(this.age >= 18)) {
+        this.openError('Debe ser mayor de 18 años', 'Intente de nuevo');
+      } else {
+        this.service.createAccount(this.newAccount);
+        this.openSuccess('Cuenta creada con éxito', 'Ok');
+        this.router.navigate(['/login']);
+      }
     }
   }
-
-  CalculateAge() {
-
-    if (this.newAccount.birthdate) {
-    var timeDiff = Math.abs(Date.now() - new Date(this.newAccount.birthdate).getTime());
-    this.age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
-    
-    console.log(this.age)
-    }
-  }
-
-
-
 }
-
